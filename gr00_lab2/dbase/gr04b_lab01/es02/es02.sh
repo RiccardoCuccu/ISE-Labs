9e70cc083e3abeae77408fbeb54e5b4aeda2af1c
Riccardo <r.cuccu@outlook.it>
2020-05-02 14:57:44 +0200

    Lab 2 - Es 1

---
+++ gr00_lab2/dbase/gr04b_lab01/es02/es02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "192.168.1.1 host1.polito.it host1
+192.168.1.32 host2.polimi.it host2
+192.168.1.33 host3.unito.it host3
+192.168.1.4 host4.unimore.it host4
+192.168.1.35 host5.tum.de host5
+192.168.1.36 host6.tum.de host6">>hostRenamed.txt
---
+++ gr00_lab2/es01.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+shopt -s extglob						# to remove files selectively
+
+##############
+#rm -r dbase
+#cp -r dbase2 dbase
+##############
+
+rm report.txt
+touch report.txt
+
+find ./dbase -name "gr[0-9]*[ab]_lab01" -type d | sort | cut -d "/" -f 3 > folders.txt
+n_lines=`wc -l ./hostRenamed.txt | cut -d " " -f 1`
+
+while read line
+do
+	folder=`echo "./dbase/"$line"/es02/"`
+	bash_file=`echo $folder"es02.sh"`
+	host_file=`echo $folder"host.txt"`
+	hostRenamed_file=`echo $folder"hostRenamed.txt"`
+	group=`echo $line | cut -d "_" -f 1`
+
+	## Correction files
+	if [ -d $folder ]									# If the folder exists, delete all files except es02.sh and host.txt
+	then
+		cd $folder
+		rm !("es02.sh"|"host.txt")
+		cd ../../..
+
+		if [ ! -e $host_file ]								# If the host.txt file does not exist, copy the original host.txt inside $folder
+		then
+			cp ./host.txt $folder/host.txt
+			echo "host.txt file missing, copy successfully in $folder"
+		elif ! cmp -s ./host.txt $folder/host.txt					# If the host.txt file has been manipulated, copy the original host.txt inside $folder
+		then
+			cp ./host.txt $folder/host.txt
+			echo "host.txt file changed, replacement successful in $folder"
+		fi
+
+		if [ -e $bash_file -a ! -x $bash_file ]						# If the es02.sh file exist and is not executable, make it executable
+		then
+			chmod u+x $bash_file
+		fi
+	fi
+
+	## Check files
+	if [ ! -d $folder ]
+	then
+		error_message="KO Missing directory es02/"
+	elif [ ! -e $bash_file ]
+	then
+		error_message="KO Missing file es02/es02.sh"
+	else
+		cd $folder
+		./es02.sh
+		cd ../../..
+
+		if [ ! -e $hostRenamed_file ]
+		then
+			error_message="KO no es02/hostRenamed.txt was generated"
+		else
+			w_lines=`diff ./hostRenamed.txt $hostRenamed_file | grep "^>" | wc -l`		# wrong lines
+			c_lines=`expr $n_lines - $w_lines`						# correct lines
+			percentage=`expr $c_lines \* 10000 / $n_lines | sed 's/..$/.&/'`		# percentage with decimals
+			
+			if [ $w_lines -ne 0 ]
+			then
+				error_message="KO Unmatched output ($percentage% correct)"
+			else
+				error_message="OK"
+			fi
+		fi
+	fi
+
+	line=`echo $group $error_message`
+	echo $line >> report.txt
+
+done < folders.txt
+
+rm folders.txt
