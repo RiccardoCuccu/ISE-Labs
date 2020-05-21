dbe1a6bb1b12bf6e716d83979832e48b47d32071
Riccardo Cuccu <57985742+RiccardoCuccu@users.noreply.github.com>
2020-05-21 09:35:26 +0200

    Update ex2.c

--- exam_20200509/ex2/ex2.c
+++ exam_20200509/ex2/ex2.c
@@ -5,38 +5,30 @@
 int main(int argc, char **argv[])
 {
 	FILE *flog;
-	float caratteri, pacchetti;
-	float temp = 0;
-	float media = 0;
-	float varianza = 0;
-	int i = 0;
-	int j = 0;
+	float caratteri[100], pacchetti[100];
+	float temp = 0, media = 0, varianza = 0;
+	int i = 0, j = 0;
 	
-	float vettore_caratteri[100];
-	float vettore_pacchetti[100];
-
 	if((flog=fopen("log.txt", "rt"))==NULL) {
 		printf("Errore nell'apertura del file");
 		exit(1);
 	}
 	
 	while(!feof(flog)) {
-		fscanf(flog,"%f\t%f\n", &caratteri, &pacchetti);
-		vettore_caratteri[i] = caratteri;
-		vettore_pacchetti[i] = pacchetti;
+		fscanf(flog,"%f\t%f\n", &caratteri[i], &pacchetti[i]);
 		i++;
 	}
 
 	for(j=0; j<i; j++) {
-		media += vettore_caratteri[j] * vettore_pacchetti[j];
+		media += caratteri[j] * pacchetti[j];
 	}
 
 	//media = media/100;
 	media = media/11;
 
 	for(j=0; j<i; j++) {
-		temp = vettore_caratteri[j] - media;
-		varianza += pow(temp,2) * vettore_pacchetti[j];
+		temp = caratteri[j] - media;
+		varianza += pow(temp,2) * pacchetti[j];
 	}
 
 	//varianza = varianza/100;
