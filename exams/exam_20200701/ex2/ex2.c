27c5352811c88d0906e714bd9da3e45b1588c1d8
Riccardo <r.cuccu@outlook.it>
2020-07-11 18:21:08 +0200

    Update exam 2020-07-01

--- exams/exam_20200701/ex2/ex2.c
+++ exams/exam_20200701/ex2/ex2.c
@@ -1,77 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 int main(int argc, char *argv[]){
 
-	FILE  *fdiv, *fout;
-	int i=0, count =0;
-	char carattere='a';                                        // variabile p
-	int aphabet[25]={0};                                    // vettore dove allocare il conteggio dei caratteri
-	int *ptr_alphabet;
-	char letters[]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};               // vettore contenente i caratteri
-	char *ptr_letters;
-	int flag=1;
-
-	if ((fdiv=fopen("divinaCommedia.txt", "rt"))==NULL) {
-		printf("Errore apertura file divinaCommedia.txt");
+	FILE *fdiv, *fout;
+	int i = 0, n = 0, count = 0;
+	char character;	
+	int aphabet[26] = {0};
+	
+	if ((fdiv=fopen("divinaCommedia.txt", "rt")) == NULL) {			// opens the divinaCommedia.txt file
+		printf("Errore apertura file divinaCommedia.txt");		// returns error if it cannot open the file
 		exit(1);
 	}
 
-	while(flag==1) {                                                    // finchè non finisce il file
-		carattere='c';
-		while (carattere!='\n' && flag==1) { // finchè non finisce la stringa             
-			if (fscanf(fdiv, "%c", &carattere) == EOF) {		// scansiona carattere per carattere
-				flag=0;
-				break;
-			}
-			printf("%c", carattere);
-
-			switch(carattere) { // switch-case in base al contenuto di carattere
-				case ' ': count=count+1; break;                           // c'è uno spazio quindi inizia una nuova parola
-				case '\n': count=count+1; break;                           // c'è una nuova riga quindi inizia una nuova parola
-				case 'A':
-				case 'a': ptr_alphabet=&aphabet[0]; aphabet[0] = *ptr_alphabet+1; break ;
-				case 'B':
-				case 'b': ptr_alphabet=&aphabet[1]; aphabet[1] = *ptr_alphabet+1; break ;
-				case 'C':
-				case 'c': ptr_alphabet=&aphabet[2]; aphabet[2] = *ptr_alphabet+1; break ;
-				case 'D':
-				case 'd': ptr_alphabet=&aphabet[3]; aphabet[3] = *ptr_alphabet+1; break ;
-				case 'E':
-				case 'e': ptr_alphabet=&aphabet[4]; aphabet[4] = *ptr_alphabet+1; break ;
-				case 'F':
-				case 'f': ptr_alphabet=&aphabet[5]; aphabet[5] = *ptr_alphabet+1; break ;
-				case 'G':
-				case 'g': ptr_alphabet=&aphabet[6]; aphabet[6] = *ptr_alphabet+1; break ;
-
-					/* faccio il case per tutte le lettere (c'è sicuramente modo di ottimizzare notevolmente e non dover fare i singoli case) */
-				case 'Z':
-				case 'z': ptr_alphabet=&aphabet[25]; aphabet[25] = *ptr_alphabet+1+1; break;
-				default: break;    // in caso di altri simboli non fa nulla
-			}
+	while (fscanf(fdiv, "%c", &character) != EOF) {				// EOF = End Of File
+		if (character == 10 || character == 32){			// if character contains a \n (ASCII=10) or a Space (ASCII=32) it means that a new word begins
+			count = count + 1;
 		}
+		else if (character >= 65 && character <= 90) {			// if character contains a value between A (ASCII=65) an Z (ASCII=90) it means that it is an uppercase
+			n = character - 65;
+			aphabet[n] += 1;
+		}
+		else if (character >= 97 && character <= 122) {			// if character contains a value between a (ASCII=97) an z (ASCII=122) it means that it is a lowercase
+			n = character - 97;
+			aphabet[n] += 1;
+		}
+
+		// First rough implementation
+		/*switch(character) {
+			case '\n':
+			case ' ': count=count + 1; break;
+			case 'A':
+			case 'a': aphabet[0] += 1; break ;
+			case 'B':
+			case 'b': aphabet[1] += 1; break ;
+			case 'C':
+			case 'c': aphabet[2] += 1; break ;
+			case 'D':
+			case 'd': aphabet[3] += 1; break ;
+			case 'E':
+			case 'e': aphabet[4] += 1; break ;
+			case 'F':
+			case 'f': aphabet[5] += 1; break ;
+			case 'G':
+			case 'g': aphabet[6] += 1; break ;
+			case 'H':
+			case 'h': aphabet[7] += 1; break ;
+			case 'I':
+			case 'i': aphabet[8] += 1; break ;
+			case 'J':
+			case 'j': aphabet[9] += 1; break ;
+			case 'K':
+			case 'k': aphabet[10] += 1; break ;
+			case 'L':
+			case 'l': aphabet[11] += 1; break ;
+			case 'M':
+			case 'm': aphabet[12] += 1; break ;
+			case 'N':
+			case 'n': aphabet[13] += 1; break ;
+			case 'O':
+			case 'o': aphabet[14] += 1; break ;
+			case 'P':
+			case 'p': aphabet[15] += 1; break ;
+			case 'Q':
+			case 'q': aphabet[16] += 1; break ;
+			case 'R':
+			case 'r': aphabet[17] += 1; break ;
+			case 'S':
+			case 's': aphabet[18] += 1; break ;
+			case 'T':
+			case 't': aphabet[19] += 1; break ;
+			case 'U':
+			case 'u': aphabet[20] += 1; break ;
+			case 'V':
+			case 'v': aphabet[21] += 1; break ;
+			case 'W':
+			case 'w': aphabet[22] += 1; break ;
+			case 'X':
+			case 'x': aphabet[23] += 1; break ;
+			case 'Y':
+			case 'y': aphabet[24] += 1; break ;
+			case 'Z':
+			case 'z': aphabet[25] += 1; break;
+			default: break;
+		}*/
 	}
 
-	printf("\n");
-	//count=count-1;				// perchè conta anche l'ultimo \n
+	fclose(fdiv);								// closes the divinaCommedia.txt file
 
-	if ((fout=fopen("report.txt", "wt"))==NULL) {            // restituisce errore se non riesco ad aprire il file
-		printf("Errore apertura file");
+	if ((fout=fopen("report.txt", "wt")) == NULL) {				// opens the report.txt file
+		printf("Errore apertura file");					// returns error if it cannot open the file
 		exit(2);
 	}
 
-	fprintf(fout, "Carattere\tOccorrenza\n");                   // stampa riga iniziale
-	for (i=0; i<26; i++) {                                        // ciclo per il numero di lettere dell'alfabeto
-		ptr_alphabet=&aphabet[i];
-		ptr_letters=&letters[i];
-		fprintf(fout, "%c\t\t%d\n", *ptr_letters, *ptr_alphabet);            // stampa il carattere e il relativo numero di occorrenze
+	fprintf(fout, "character\tOccorrenza\n");				// print the start line
+	for (i=0; i<26; i++) {							// print each letter and its occurrences
+		n=97+i;
+		fprintf(fout, "%c\t\t%d\n", n, aphabet[i]);
 	}
 
-	fprintf(fout, "Totale parole: %d", count);                // stampa il totale delle parole
+	fprintf(fout, "Totale parole: %d", count);				// print the number of words
 
-	fclose(fout);
-	fclose(fdiv);
-	
+	fclose(fout);								// closes the report.txt file
+
 	return 0;
 }
