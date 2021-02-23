2bc91bf8ec250a6e99028b0362e7c6c4be332efc
Riccardo <r.cuccu@outlook.it>
2021-02-23 09:37:25 +0100

    Corrected algorithm

--- exams/exam_20210205/ex1/ex1.py
+++ exams/exam_20210205/ex1/ex1.py
@@ -3,12 +3,14 @@
 # Import the simulator module (it should be in the same directory as this program)
 import fakeSerial as serial
 
+MAX_SAMPLES = 16
+
 average = 0
 i = 0								# data counter
-L = []								# list of converted hexadecimal characters
+L = [0]*MAX_SAMPLES						# list of converted hexadecimal characters
 
-sin = serial.Serial(0)						# open serial 0 (simulated)
-sout = serial.Serial(1)						# open serial 1 (simulated)
+sin = serial.Serial(port = '/dev/ttyS0', baudrate = 9600)	# open serial /dev/ttyS0 (simulated)
+sout = serial.Serial(port = '/dev/ttyS1', baudrate = 9600)	# open serial /dev/ttyS1 (simulated)
 
 try:
 	f = open("data.txt", "w")				# open file for writing
@@ -17,18 +19,18 @@ except:
 	exit(-1)
 
 while True:
-	dato = sin.read(2)					# read 16 bits
+	dato = sin.read(2)					# read 16 bits (2 bytes)
 	if dato == '': break					# check if the data stream is over
 
-	if i >= 16:
+	if i >= MAX_SAMPLES:
 		i = 1						# reset data counter
-		L = []						# reset datas
 	else:
 		i += 1						# increment data counter
 	
-	L.append(int(dato, 16))					# concatenate the data received in decimal
+	L.pop(i-1)						# remove the oldest data
+	L.insert(i-1, int(dato, 16))				# convert the received data into decimal and enter it in the list
 	average = sum(L)					# sum all elements of the list
-	average = average // i					# update the moving average (only integer values)
+	average = average // MAX_SAMPLES			# update the moving average (only integer values)
 
 	f.write(format(average, 'x') + "\n")			# write to file
 	sout.write(format(average, 'x'))			# write to serial
