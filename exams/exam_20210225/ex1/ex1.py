b4e674e7cfd4868f5bec3f2bc8a4977f3cc07d68
Riccardo <r.cuccu@outlook.it>
2021-02-27 18:15:04 +0100

    Update ex1 exam 2021-02-25

---
+++ exams/exam_20210225/ex1/ex1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Import the simulator module (it should be in the same directory as this program)
+import fakeSerial as serial
+
+INTBIT = 12							# number of bits to represent an unsigned integer
+HEXDIGIT = int(INTBIT/4)					# number of hexadecimal characters to represent the same unsigned integers
+BYTETOREAD = HEXDIGIT*2+1					# total number of bytes to read from the serial for each operation
+
+sin = serial.Serial(port = '/dev/ttyS0', baudrate = 9600)	# open serial /dev/ttyS0 (simulated)
+sout = serial.Serial(port = '/dev/ttyS1', baudrate = 9600)	# open serial /dev/ttyS1 (simulated)
+
+try:
+	f = open("data.txt", "w")				# open file for writing
+except:
+	print("ERROR: failed to open output file!")
+	exit(-1)
+
+while(1):
+	dato = sin.read(BYTETOREAD)				# read 56 bits (7 bytes)
+	if dato == '': break					# check if the data stream is over
+
+	n1 = int(dato[0:3], 16)					# convert the first hex to decimal
+	n2 = int(dato[3:6], 16)					# convert the second hex to decimal
+	op = dato[6]						# extract the bitwise operation
+
+	if (op == '+'):						# OR operation
+		result = n1 | n2
+	elif (op == 'x'):					# AND operation
+		result = n1 & n2
+	elif (op == '#'):					# XOR operation
+		result = n1 ^ n2
+
+	f.write('%d %c %d %d\n' % (n1, op, n2, result))		# write to file
+	sout.write('%x' % result)				# write to serial
+
+sin.close()							# close serial /dev/ttyS0
+sout.close()							# close serial /dev/ttyS1
+f.close()							# close file
---
+++ exams/exam_20210225/ex1/fakeSerial.py
@@ -0,0 +1,88 @@
+# fakeSerial.py
+# D. Thiebaut
+# A very crude simulator for PySerial assuming it
+# is emulating an Arduino.
+
+# Modified by R. Cuccu
+
+
+# a Serial class emulator 
+class Serial:
+
+	## init(): the constructor.  Many of the arguments have default values
+	# and can be skipped when calling the constructor.
+	def __init__( self, port='COM1', baudrate = 9600, timeout=1, bytesize = 8, parity = 'N', stopbits = 1, xonxoff=0, rtscts = 0):
+		self.name     = port
+		self.port     = port
+		self.timeout  = timeout
+		self.parity   = parity
+		self.baudrate = baudrate
+		self.bytesize = bytesize
+		self.stopbits = stopbits
+		self.xonxoff  = xonxoff
+		self.rtscts   = rtscts
+		self._isOpen  = True
+		self._receivedData = ""
+		#self._data = "It was the best of times.\nIt was the worst of times.\n"
+		self._data = "012345+6789ABxCDEF01#234567+89ABCDxEF0123#456789+ABCDEFx012345#6789AB+CDEF01x234567#"
+
+	## isOpen()
+	# returns True if the port to the Arduino is open.  False otherwise
+	def isOpen( self ):
+		return self._isOpen
+
+	## open()
+	# opens the port
+	def open( self ):
+		self._isOpen = True
+
+	## close()
+	# closes the port
+	def close( self ):
+		self._isOpen = False
+
+	## write()
+	# writes a string of characters to the Arduino
+	def write( self, string ):
+		print( 'Arduino got: "' + string + '"' )
+		self._receivedData += string
+
+	## read()
+	# reads n characters from the fake Arduino. Actually n characters
+	# are read from the string _data and returned to the caller.
+	def read( self, n=1 ):
+		s = self._data[0:n]
+		self._data = self._data[n:]
+		#print( "read: now self._data = ", self._data )
+		return s
+
+	## readline()
+	# reads characters from the fake Arduino until a \n is found.
+	def readline( self ):
+		returnIndex = self._data.index( "\n" )
+		if returnIndex != -1:
+			s = self._data[0:returnIndex+1]
+			self._data = self._data[returnIndex+1:]
+			return s
+		else:
+			return ""
+
+	## read_until()
+	# reads characters from the fake Arduino until a specific string of characters is found.
+	def read_until( self, string ):
+		returnIndex = self._data.index( string )
+		if returnIndex != -1:
+			s = self._data[0:returnIndex+1]
+			self._data = self._data[returnIndex+1:]
+			return s
+		else:
+			return ""
+
+	## __str__()
+	# returns a string representation of the serial class
+	def __str__( self ):
+		return  "Serial<id=0xa81c10, open=%s>( port='%s', baudrate=%d," \
+			   % ( str(self.isOpen), self.port, self.baudrate ) \
+			   + " bytesize=%d, parity='%s', stopbits=%d, xonxoff=%d, rtscts=%d)"\
+			   % ( self.bytesize, self.parity, self.stopbits, self.xonxoff,
+				   self.rtscts )
