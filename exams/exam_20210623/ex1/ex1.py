5d7d94a3dd22ca581ddf973f5d568513b143be1b
Riccardo <r.cuccu@outlook.it>
2021-08-28 18:20:11 +0200

    Optimizations

--- exams/exam_20210623/ex1/ex1.py
+++ exams/exam_20210623/ex1/ex1.py
@@ -1,37 +1,48 @@
 #!/usr/bin/env python3
 
+DIGIT = 4
 SAMPLES = 16
 
 try:
-	fin = open('input.txt', 'r')				# open file for reading
+	fin = open('input.txt', 'r')						# open file for reading
 except:
 	print("ERROR: failed to open input file!")
 	exit(-1)
 
 try:
-	fout = open('output.txt','w')				# open file for writing
+	fout = open('output.txt','w')						# open file for writing
 except:
 	print("ERROR: failed to open output file!")
 	exit(-1)
 
-content = fin.read()						# extract the content of the input file
-numbers = content.split(' ')					# conversion from string to list
+L = [0]*DIGIT									# list of digits
+N = [0]*SAMPLES									# list of samples
+i = 0										# digits counter
+j = 0										# samples counter
+n = ""										# string number
 
-i = 0								# counter
-average = 0							# average
-values = []							# list of samples
+while True:
+	ch = fin.read(1)							# reads one character at a time				
 
-for number in numbers:						# for each number contained in the input file
-	if i == SAMPLES:
-		average = sum(values)				# sum the samples
-		average = int(average/SAMPLES)			# average calculation
-		fout.write(str(format(average, '04x') + ' '))	# writing the 4-digit hexadecimal value (16 bit) to the output file
-		values = []					# reset of samples
-		values.append(int(number, 10)) 			# conversion from character to integer
-		i = 1						# reset samples counter
+	if ch == '': break							# EOF
+	elif ch == ' ':								# if the character is a space the number is finished
+		for digit in L:							# concatenate digits
+			if digit == 0: break
+			n = n + digit
+
+		N[j] = int(n)							# conversion from string to integer
+		j += 1								# increment samples counter
+		i = 0								# reset digit counter
+		n = ""								# reset number
+		L = [0]*DIGIT							# delete digits
 	else:
-		values.append(int(number, 10))			# conversion from character to integer
-		i = i+1						# increment samples counter
+		L[i] = ch							# save digit
+		i += 1								# increment digit counter
 
-fin.close()							# close input file
-fout.close()							# close output file
+	if j == SAMPLES:
+		average = int(sum(N) / SAMPLES)					# average calculation
+		fout.write(str(format(average, '0'+str(DIGIT)+'x')) + ' ')	# writing the 4-digit hexadecimal value (16 bit) to the output file
+		j = 0								# reset samples counter
+
+fin.close()									# close input file
+fout.close()									# close output file
